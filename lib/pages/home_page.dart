652ff0d9c398b1de235b412a7e65c4f18f2ce21a
Swastik Patel <dawnsp0456@gmail.com>
2024-05-31 03:19:41 +0530

    darwer skelton

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -34,6 +34,126 @@ class _HomePageState extends State<HomePage> {
       bottomNavigationBar: XDBottomNavBar(
         onTabChange: (index) => navigateBottomBar(index),
       ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Builder(builder: (context) {
+          return IconButton(
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+          );
+        }),
+      ),
+      drawer: Drawer(
+        backgroundColor: Color.fromARGB(255, 19, 36, 46),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                DrawerHeader(
+                  child: SizedBox(
+                    height: 200,
+                    child: Image.asset(
+                      'lib/images/Logo.png',
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: Divider(
+                    color: Colors.white,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 25.0, top: 5),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.home,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      'Home',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.trending_up,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      'Trending',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.add_box,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      'Most Wished',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.headset_mic,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  'Live Support',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.info,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  'About',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(left: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.login,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  'Log Out',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
       body: _pages[_selectedIndex],
     );
   }
